1fc8dc261f5fea300b47735bc6199d0d5268c9c5
Zhukovskiy Stanislav <menistrel2005@yandex.ru>
2017-10-07 20:00:48 +0300

    add project

--- gulp/paths/app.js
+++ gulp/paths/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = [
+  './source/js/demo.js',
   './source/js/app.js'
 ];
--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = [
-  './node_modules/jquery/dist/jquery.min.js'
+  './node_modules/jquery/dist/jquery.min.js',
+  './node_modules/charming/charming.min.js',
+  './node_modules/animejs/anime.min.js',
+  './node_modules/imagesloaded/imagesloaded.pkgd.min.js'
 ];
